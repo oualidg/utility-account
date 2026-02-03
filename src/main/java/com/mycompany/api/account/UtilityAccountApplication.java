6c036529764b2e92600ef79273dfa64e0f9cb742
Oualid Gharach <oualid.gharach@gmail.com>
2026-02-03 17:05:58 +0200

    Initial setup: Spring Boot + Actuator + GitHub Pages

---
+++ src/main/java/com/mycompany/api/account/UtilityAccountApplication.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2026 Oualid Gharach. All rights reserved.
+ *
+ * Aiming for production-grade standards through clean code and best practices
+ * for educational and informational purposes.
+ *
+ * Created on: 2/3/2026 at 4:49 PM
+ *
+ * Feel free to use or contribute. Contact: oualid.gharach@gmail.com
+ */
+package com.mycompany.api.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilityAccountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtilityAccountApplication.class, args);
+    }
+
+}
